c4ca757d00cc875a2d197ce2d4a576e11b22c1f9
fengzizu <2225942674@qq.com>
2018-01-30 14:05:46 +0800

    测试

--- src/com/kjlink/privilege/util/UUIDUtils.java
+++ src/com/kjlink/privilege/util/UUIDUtils.java
@@ -1,14 +1,14 @@
-package com.kjlink.privilege.util;
+﻿package com.kjlink.privilege.util;
 
 import java.util.UUID;
 
 public class UUIDUtils {
+
 	/**
 	 * 随机生成id
-	 * @return
 	 */
 	public static String getId(){
-		return UUID.randomUUID().toString().replace("-", "").toUpperCase();
+		return UUID.randomUUID().toString().replace("-", "").toUpperCase(); 
 	}
 	
 	
